ca0a5cb7b14d8736bbe456a17d2a666d62c6cc01
Jackson Davis <jdavis3695@gmail.com>
2023-01-05 23:38:09 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 const app = express()
 const db = require('./queries')
 const port = 3000
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
