e087d84071a3ba2a6cf6b149676d158fb5d56a2f
Jackson Davis <jdavis3695@gmail.com>
2023-01-06 05:32:30 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   queries.js

--- queries.js
+++ queries.js
@@ -1,5 +1,5 @@
 const pool = require('./dbInfo').getPool();
-const requestcall = require('request');
+const axios = require('axios');
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) {
@@ -73,11 +73,36 @@ const getDataDump = (request, response) => {
 
 const createDataDump = (request, response) => {
     const { title, text } = request.body;
-    requestcall('http://www.google.com', function (error, resp, body) {
-        if (!error && resp.statusCode == 200) {
-            console.log('woo') // Print the google web page.
-        }
-        pool.query('INSERT INTO datadump(title, text, created, last_updated) VALUES ($1, $2, NOW(), NOW())', [title, text], (error, results) => {
+    const client_id = "1kcquaftbv249cuwslv5rmbasotdc4";
+    const bearer = "Bearer 3vujqhiblzmx2ddbelpc08h327lh4m"
+    const url = "https://api.igdb.com/v4/games/"
+    let summary = "";
+    let titl = "";
+    const populate = (tit, sum) => { summary = sum; titl = tit; }
+    let config = {
+        method: 'POST',
+        url: url,
+        headers: {
+            'Accept': 'application/json',
+            'Client-ID': client_id,
+            'Authorization': bearer
+        },
+        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+    }
+    async function getData() {
+        await axios(config)
+            .then(response => {
+                populate(response.data[0]['name'], response.data[0]['summary']);
+                console.log(titl, summary);
+                insertData();
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    getData();
+    const insertData = () => {
+        pool.query('INSERT INTO datadump(title, text, created, last_updated) VALUES ($1, $2, NOW(), NOW())', [titl, summary], (error, results) => {
             if (error) {
                 response.status(401).send(`data in table already`);
             }
@@ -85,7 +110,7 @@ const createDataDump = (request, response) => {
                 response.status(201).send(`data added to the table.`);
             }
         })
-    })
+    }
 }
 module.exports = {
     createDataDump,
